3069b2442c47809f01f1acacc2654d235ef9ee34
Iqbal Tawakkal <iqbaltawakkal1@gmail.com>
2024-08-31 08:30:46 +0700

    fix: editor script and query

--- src/components/Scheduler.vue
+++ src/components/Scheduler.vue
@@ -22,17 +22,17 @@ const props = defineProps({
 onBeforeMount(() => {
   let script = wwLib.getFrontDocument().createElement("script");
   script.type = "module";
-  script.id = "nylas-scheduler-id"
+  script.id = "nylas-scheduler-id-editor"
   script.defer = true;
   script.src = "https://unpkg.com/@nylas/web-elements@1.1.1/dist/nylas-web-elements/nylas-web-elements.esm.js";
-  if (wwLib.getFrontDocument().getElementById('nylas-scheduler-id')) {
-    wwLib.getFrontDocument().getElementById('nylas-scheduler-id').remove();
+  if (wwLib.getFrontDocument().getElementById('nylas-scheduler-id-editor')) {
+    wwLib.getFrontDocument().getElementById('nylas-scheduler-id-editor').remove();
   }
   wwLib.getFrontDocument().head.appendChild(script);
 })
 
 onMounted(() => {
-  const nylasSchedulerEditor = document.querySelector('nylas-scheduler-editor')
+  const nylasSchedulerEditor = wwLib.getFrontDocument().querySelector('nylas-scheduler-editor')
   nylasSchedulerEditor.schedulerPreviewLink = `${props.urlDomain}/?config_id=${props.configurationId}`
   nylasSchedulerEditor.nylasSessionsConfig = {
     clientId: props.clientId,
